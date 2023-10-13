9315d3cb43a9a9ddf93a37df73fca9900f7e3927
Lucas Veras <lucasverasspineli@gmail.com>
2023-10-12 23:43:36 -0300

    Create class UserController

---
+++ src/main/java/br/com/lucasveras/todolist/user/UserController.java
@@ -0,0 +1,14 @@
+package br.com.lucasveras.todolist.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    public void create(){
+        
+    }
+
+}
