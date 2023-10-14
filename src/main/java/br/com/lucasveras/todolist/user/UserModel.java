87b0f49db207e528b41896ba6e6c0eda0e98e6ea
Lucas Veras <lucasverasspineli@gmail.com>
2023-10-14 20:52:28 -0300

    Created table tb_users

--- src/main/java/br/com/lucasveras/todolist/user/UserModel.java
+++ src/main/java/br/com/lucasveras/todolist/user/UserModel.java
@@ -1,12 +1,25 @@
 package br.com.lucasveras.todolist.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
+@Entity(name = "tb_users")
 public class UserModel {
-
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
     private String userName;
     private String name;
     private String password;
 
+    //Data de quando o dado for criado
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
