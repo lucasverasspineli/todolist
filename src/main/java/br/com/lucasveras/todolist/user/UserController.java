16a91f76e58ad0e36775624755207780adf909e9
Lucas Veras <lucasverasspineli@gmail.com>
2023-10-15 01:56:32 -0300

    Create cryptography for password

--- src/main/java/br/com/lucasveras/todolist/user/UserController.java
+++ src/main/java/br/com/lucasveras/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.lucasveras.todolist.user;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,10 @@ public class UserController {
         if(user != null){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Já existe este usuário!");
         }
+        // Criptografando a senha
+        var passwordCrypt= BCrypt.withDefaults().hashToString(12,userModel.getPassword().toCharArray());
+        userModel.setPassword(passwordCrypt);
+
         var userCreated = this.iUserRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
