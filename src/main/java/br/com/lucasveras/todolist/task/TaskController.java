37d3dde4de357dc16ef71aa2c254f7321fa5ae9a
Lucas Veras <lucasverasspineli@gmail.com>
2023-10-16 00:12:08 -0300

    Created method listTask for list all taks in the user

--- src/main/java/br/com/lucasveras/todolist/task/TaskController.java
+++ src/main/java/br/com/lucasveras/todolist/task/TaskController.java
@@ -4,12 +4,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -20,7 +18,7 @@ public class TaskController {
 
     @PostMapping("/")
     public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request){
-        System.out.println("Chegou no controller "+request.getAttribute("idUser"));
+//        System.out.println("Chegou no controller "+request.getAttribute("idUser"));
         var idUser = request.getAttribute("idUser");
         taskModel.setIdUser((UUID) idUser);
         //Validação para datas
@@ -34,5 +32,11 @@ public class TaskController {
         var task = this.iTaskRepository.save(taskModel);
         return ResponseEntity.status(HttpStatus.OK).body(task);
     }
+    @GetMapping("/")
+    public List<TaskModel> ListTask(HttpServletRequest request){
+        var idUser = request.getAttribute("idUser");
+        var task = iTaskRepository.findByIdUser((UUID) idUser);
+        return task;
 
+    }
 }
