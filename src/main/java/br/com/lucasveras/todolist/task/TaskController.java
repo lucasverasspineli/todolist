f334e791e2791d014222e944a656c07a466e25f8
Lucas Veras <lucasverasspineli@gmail.com>
2023-10-16 23:11:56 -0300

    Validation the user for task update

--- src/main/java/br/com/lucasveras/todolist/task/TaskController.java
+++ src/main/java/br/com/lucasveras/todolist/task/TaskController.java
@@ -42,14 +42,22 @@ public class TaskController {
 
     }
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
-        var idUser = request.getAttribute("idUser");
+    public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
 
         var task = this.iTaskRepository.findById(id).orElse(null);
+        if(task == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa não encontrada");
+        }
+        var idUser = request.getAttribute("idUser");
+
+        if(!task.getIdUser().equals(idUser)){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("O usuário não tem permissão para alterar a tarefa");
+        }
 
         Utils.copyNonNullProperties(taskModel, task);
+        var taskUpdate = this.iTaskRepository.save(task);
 
-        return this.iTaskRepository.save(task);
+        return ResponseEntity.ok().body(taskUpdate);
 
     }
 }
