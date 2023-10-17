dc19ce54feb8e2105f34fed966489737a949ec08
Lucas Veras <lucasverasspineli@gmail.com>
2023-10-16 23:27:52 -0300

    Created class ExceptionHandlerController for validation for titles longer than 50 characters

--- src/main/java/br/com/lucasveras/todolist/task/TaskModel.java
+++ src/main/java/br/com/lucasveras/todolist/task/TaskModel.java
@@ -25,4 +25,12 @@ public class TaskModel {
     private UUID idUser;
     @CreationTimestamp
     private LocalDateTime createdAt;
+
+    public void setTitle(String title) throws Exception {
+        if(title.length() > 50){
+            throw new Exception("O campo deve conter no máximo 50 caracteres");
+        }
+        this.title = title;
+    }
+
 }
